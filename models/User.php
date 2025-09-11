f7b33e0038e856d0e6de38cef95d19a3a738e7ac
Sandro <sandrosqrt@gmail.com>
2025-09-11 14:28:48 -0300

    Creating image name generator function

--- models/User.php
+++ models/User.php
@@ -18,6 +18,10 @@
             return password_hash($password, PASSWORD_DEFAULT);
         }
 
+        public function imageGenerateName() {
+            return bin2hex(random_bytes(60));
+        }
+
         public function getFullName($user) {
             return $user->name . " " . $user->lastname;
         }
