2a9b02ffa20492162db646b9e44a52d46d321ce3
Sandro <sandrosqrt@gmail.com>
2025-10-15 00:23:39 -0300

    Criando model do Objeto

---
+++ models/Object.php
@@ -0,0 +1,27 @@
+<?php
+
+class Object {
+
+    public $id;
+    public $title;
+    public $image;
+    public $description;
+    public $users_id;
+
+}
+
+
+interface ObjectDAOInterface {
+
+    public function buildObject($data);
+    public function create(Object $object);
+    public function update(Object $object);
+    public function delete($id);
+    public function findAll();
+    public function findById($id);
+    public function findByTitle($title);
+    public function findByUserId($id);
+    public function findLatestObjects();
+
+
+  }
