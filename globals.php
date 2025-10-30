8d33e03d8f4a9eeb2c8df6db1ae575e5b605f8a9
Sandro <sandrosqrt@gmail.com>
2025-10-30 16:45:19 -0300

    Fixing CSS

--- globals.php
+++ globals.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
-$BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/';
-$BASE_URL = str_replace('index.php', '', $BASE_URL);
-$BASE_URL = rtrim($BASE_URL, '/') . '/';
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host = $_SERVER['HTTP_HOST'];
+
+$root = str_replace(['/views', '/templates'], '', dirname($_SERVER['SCRIPT_NAME']));
+$root = rtrim($root, '/\\') . '/';
+
+$BASE_URL = $protocol . $host . $root;
